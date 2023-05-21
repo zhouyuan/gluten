287d5598bd5214b7f76db527a0fa43b81bc2d57a
Yuan Zhou <yuan.zhou@intel.com>
2023-05-22 07:40:41 +0800

    enable io executor pool and s3 write
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- cpp/velox/compute/VeloxParquetDatasource.cc
+++ cpp/velox/compute/VeloxParquetDatasource.cc
@@ -58,6 +58,14 @@ void VeloxParquetDatasource::init(const std::unordered_map<std::string, std::str
         "The write path is hdfs path but the HDFS haven't been enabled when writing parquet data in velox backend!");
 #endif
 
+  }  else if (strncmp(filePath_.c_str(), "s3a:", 4) == 0 || strncmp(filePath_.c_str(), "s3n:", 4) == 0) {
+#ifdef ENABLE_S3
+    sink_ = std::make_unique<velox::S3FileSink>(filePath_);
+#else
+    throw std::runtime_error(
+        "The write path is S3 path but the S3 haven't been enabled when writing parquet data in velox backend!");
+#endif
+
   } else {
     throw std::runtime_error(
         "The file path is not local or hdfs when writing data with parquet format in velox backend!");
