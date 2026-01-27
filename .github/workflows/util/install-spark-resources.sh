3ddd36b58d3cf0760ca7addd3c9bc773fa41f472
Yuan <yuanzhou@apache.org>
2026-01-27 13:34:16 +0000

    [VL] Remove tempeorary spark source installation
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- .github/workflows/util/install-spark-resources.sh
+++ .github/workflows/util/install-spark-resources.sh
@@ -113,11 +113,13 @@ case "$1" in
     # Spark-4.0, scala 2.12 // using 2.12 as a hack as 4.0 does not have 2.13 suffix
     cd ${INSTALL_DIR} && \
     install_spark "4.0.1" "3" "2.12"
+    mv /opt/shims/spark40/spark_home/assembly/target/scala-2.12 /opt/shims/spark40/spark_home/assembly/target/scala-2.13
     ;;
 4.1)
     # Spark-4.x, scala 2.12 // using 2.12 as a hack as 4.0 does not have 2.13 suffix
     cd ${INSTALL_DIR} && \
     install_spark "4.1.1" "3" "2.12"
+    mv /opt/shims/spark41/spark_home/assembly/target/scala-2.12 /opt/shims/spark41/spark_home/assembly/target/scala-2.13
     ;;
 *)
     echo "Spark version is expected to be specified."
