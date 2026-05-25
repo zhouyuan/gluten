3289a34d945ddf4b8fe112549d8409a9154390d4
Yuan <yuanzhou@apache.org>
2026-05-25 15:43:31 +0100

    fix

--- dev/vcpkg/triplets/x64-linux-avx-dynamic-openssl.cmake
+++ dev/vcpkg/triplets/x64-linux-avx-dynamic-openssl.cmake
@@ -1,4 +1,4 @@
-include("${CMAKE_CURRENT_LIST_DIR}/x64-linux-avx-base.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/x64-linux-avx.cmake")
 
 if("${PORT}" STREQUAL "openssl")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
